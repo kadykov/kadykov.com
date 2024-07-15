858735a03823ddf1696c9fdf276c58bf2643285e
Aleksandr Kadykov <git@kadykov.com>
2024-07-15 23:44:18 +0200

    Minimal draft with favicon and Picture

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		icon()
+	],
+	site: 'https://kadykov.com',
+	image: {
+		domains: ["kadykov.com"],
+		remotePatterns: [{
+			protocol: "https"
+		}]
+	}
 });
