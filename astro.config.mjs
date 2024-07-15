e9bf718b970f8e3d42d4b1b9382bb6f85ad1e9d0
Aleksandr Kadykov <git@kadykov.com>
2024-07-16 01:26:33 +0200

    Add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		icon()
-	],
-	site: 'https://kadykov.com',
-	image: {
-		domains: ["kadykov.com"],
-		remotePatterns: [{
-			protocol: "https"
-		}]
-	}
+  integrations: [tailwind(), icon(), sitemap()],
+  site: 'https://kadykov.com',
+  image: {
+    domains: ["kadykov.com"],
+    remotePatterns: [{
+      protocol: "https"
+    }]
+  }
 });
