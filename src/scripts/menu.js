8d639be89810eec033ee9d201226026ea463c2d4
Aleksandr Kadykov <git@kadykov.com>
2024-08-08 01:17:10 +0200

    Implement collapsable navigation

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,3 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('nav').classList.toggle('collapse');
+    document.querySelector('nav').classList.toggle('hidden');
 });
