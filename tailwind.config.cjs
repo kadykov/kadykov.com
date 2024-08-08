f02c107d893793e510131e12f57969493bfcf159
Aleksandr Kadykov <git@kadykov.com>
2024-08-09 00:33:31 +0200

    Add martel and mulish fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['"Mulish Variable"', ...defaultTheme.fontFamily.sans],
+				serif: ['"Martel"', ...defaultTheme.fontFamily.serif],
+			},
+		},
 	},
 	plugins: [],
 };
