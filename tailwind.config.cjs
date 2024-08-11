8671c7cf8446ef2fee790a3dcc457be488cff9f8
Aleksandr Kadykov <git@kadykov.com>
2024-08-11 13:48:05 +0200

    Use build-in tailwind fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,6 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			fontFamily: {
-				sans: ['"Overpass Variable"', ...defaultTheme.fontFamily.sans],
-				serif: ['"Lora Variable"', ...defaultTheme.fontFamily.serif],
-			},
 		},
 	},
 	plugins: [],
