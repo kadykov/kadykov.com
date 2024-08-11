096ae6cb4e5f611eb25d9600123dab16446e49c6
Aleksandr Kadykov <git@kadykov.com>
2024-08-12 00:19:21 +0200

    Add MDX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({}), icon(), sitemap()],
+  integrations: [tailwind({}), icon(), sitemap(), mdx()],
   site: "https://www.kadykov.com",
   image: {
     domains: ["kadykov.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-      },
-    ],
-  },
+    remotePatterns: [{
+      protocol: "https"
+    }]
+  }
 });
