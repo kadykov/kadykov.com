2ed1897d5255946c808a2d2e3073a56780280d8a
Aleksandr Kadykov <git@kadykov.com>
2024-08-18 01:02:04 +0200

    Use intermediate breakpoints, fix navigation alignment

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,14 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    screens: {
+      // From https://www.freecodecamp.org/news/the-100-correct-way-to-do-css-breakpoints-88d6a5ba1862/
+      sm: "600px",
+      md: "900px",
+      lg: "1200px",
+      xl: "1500px",
+      "2xl": "1800px",
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
