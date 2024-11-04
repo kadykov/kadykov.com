18370e07f7c1ae4495de504d7e9a86a2d1c0daca
Aleksandr Kadykov <git@kadykov.com>
2024-11-04 23:50:45 +0000

    Use IBM Plex Serif for headings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+				serif: ['"IBM Plex Serif"', ...defaultTheme.fontFamily.serif],
+			},
+    },
     screens: {
       // From https://www.freecodecamp.org/news/the-100-correct-way-to-do-css-breakpoints-88d6a5ba1862/
       sm: "600px",
