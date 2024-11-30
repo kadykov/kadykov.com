2ada918915ed2ad0811868865f63ec1a5ec033e6
Aleksandr Kadykov <git@kadykov.com>
2024-11-30 23:17:19 +0000

    Add self-portrait gallery prototype with glightbox

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,7 +15,23 @@ const postsCollection = defineCollection({
     tags: z.array(z.string()),
   }),
 })
+
+const galleriesCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    images: z.array(
+      z.object({
+        src: z.string().url(), // Validate as a URL
+        alt: z.string(),
+        title: z.string().optional(), // Optional title
+        description: z.string().optional(), // Optional description
+      })
+    ),
+  }),
+})
+
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   posts: postsCollection,
+  galleries: galleriesCollection,
 }
---
+++ src/scripts/lightbox.js
@@ -0,0 +1,20 @@
+import Glightbox from "glightbox"
+import "glightbox/dist/css/glightbox.min.css"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const lightbox = Glightbox({
+    touchNavigation: true,
+    loop: true,
+    closeButton: true,
+    width: "100vw",
+    height: "100vh",
+    preload: true,
+    openEffect: "fade",
+    closeEffect: "fade",
+    slideEffect: "fade",
+    dragAutoSnap: true,
+    cssEfects: {
+      fade: { in: "fadeIn", out: "fadeOut" },
+    },
+  })
+})
