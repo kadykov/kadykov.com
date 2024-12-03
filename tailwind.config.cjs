783eada405e4ef208fedb9c87390901ccce5dde2
Aleksandr Kadykov <git@kadykov.com>
2024-12-03 00:33:22 +0000

    Switch to fantasy and dracula themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,13 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme"
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ['"Fira Sans"', ...defaultTheme.fontFamily.sans],
-				serif: ['"IBM Plex Serif"', ...defaultTheme.fontFamily.serif],
-				mono: ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono],
-			},
+        serif: ['"IBM Plex Serif"', ...defaultTheme.fontFamily.serif],
+        mono: ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono],
+      },
     },
     screens: {
       // From https://www.freecodecamp.org/news/the-100-correct-way-to-do-css-breakpoints-88d6a5ba1862/
@@ -18,8 +18,9 @@ module.exports = {
       "2xl": "1800px",
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require('daisyui'),
-  ],
-};
+  daisyui: {
+    themes: ["fantasy", "dracula",],
+    darkTheme: "dracula",
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+}
