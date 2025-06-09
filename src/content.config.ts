48b060a37afa0d1c13387d6ff873c78981c4cdcd
Aleksandr Kadykov <git@kadykov.com>
2025-06-09 23:37:06 +0000

    feat: remove self-portrait gallery and associated content; streamline collections configuration

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,25 +16,7 @@ const postsCollection = defineCollection({
   }),
 })
 
-const galleriesCollection = defineCollection({
-  type: "data", // For JSON/YAML files in src/content/
-  schema: z.object({
-    images: z.array(
-      z.object({
-        src: z.string().url(), // Validate as a URL
-        alt: z.string(),
-        title: z.string().optional(), // Optional title
-        description: z.string().optional(), // Optional description
-      })
-    ),
-  }),
-})
-// Note: If galleries are also to be moved to src/content/galleries,
-// the loader would be removed and type: "data" would be used.
-// For now, only modifying postsCollection as per immediate plan.
-
 // Export a single `collections` object to register your collection(s)
 export const collections = {
-  blog: postsCollection,
-  galleries: galleriesCollection,
+  blog: postsCollection
 }
